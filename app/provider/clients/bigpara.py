86c9fd8765ab3f2b361d2c3c844de19678062024
Mesut Öncel <mesut.oncel@metglobal.com>
2019-02-20 15:43:14 +0300

    refactoring client & add  dataclass for error and response

--- app/provider/clients/bigpara.py
+++ app/provider/clients/bigpara.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import json
+from app.provider.clients.helpers import client_response_validation
 
 
 class Client:
@@ -14,13 +15,16 @@ class Client:
         return requests.request(method=method, url=url,
                                 data=json.dumps(post_data))
 
+    @client_response_validation
     def extract_stock(self, stock_symbol):
         url = '{}-detay/teknik-yorum/'.format(stock_symbol)
         return self.make_request(url=url)
 
+    @client_response_validation
     def extract_all_stock(self):
         return self.make_request(url='')
 
+    @client_response_validation
     def stock_table_data(self, link):
         url = [item for item in link.split('/') if item != ''][-1]
         return self.make_request(url=url)
--- app/provider/clients/uzmanpara.py
+++ app/provider/clients/uzmanpara.py
@@ -3,6 +3,8 @@ import json
 import requests
 import logging
 
+from app.provider.clients.helpers import client_response_validation
+
 logger = logging.getLogger('main')
 
 
@@ -18,14 +20,17 @@ class Client:
         return requests.request(method=method, url=url,
                                 data=json.dumps(post_data))
 
+    @client_response_validation
     def extract_stock(self, stock_symbol):
         url = os.path.join('borsa/hisse-senetleri', stock_symbol.upper())
         return self.make_request(url=url)
 
+    @client_response_validation
     def extract_all_stock(self):
         url = 'canli-borsa/'
         return self.make_request(url=url)
 
+    @client_response_validation
     def stock_table_data(self, link):
         url = [item for item in link.split('/') if item != ''][-1]
         return self.make_request(url=url)
