01a9c4441a73d4cfa6ca6d65dd036aa1f86e4b77
Mesut Öncel <mesutoncel91@gmail.com>
2019-03-01 11:38:21 +0300

    Add celery

--- app/api/views.py
+++ app/api/views.py
@@ -45,6 +45,7 @@ class StockView(APIView):
                 response.update(dict(
                     provider=provider.pk
                 ))
+
         serializer = StockSerializer(data=response_data, many=True)
         if serializer.is_valid(raise_exception=True):
             return Response(serializer.data, status=status.HTTP_200_OK)
